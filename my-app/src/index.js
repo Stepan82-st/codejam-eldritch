d8c0de109a56b50f90efe1e190b318ab0db0dffc
Stepan Bryl <bryl.step@tut.by>
2022-08-25 15:46:08 +0300

    init button

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,4 +1,4 @@
-import image from './images/lazy.png';
+//import image from './images/lazy.png';
 
 const createImage = (src) => new Promise((res, rej) => {
   const img = new Image();
@@ -16,3 +16,42 @@ async function render() {
 }
 
 //render();
+const mapsAzathoth = document.querySelectorAll('.maps');
+const btn = document.getElementsByTagName('button');
+console.log(btn);
+mapsAzathoth.forEach(elem => 
+elem.addEventListener('click', function(){
+  const btnLeave = document.querySelectorAll('.btn-leave');
+  btnLeave.forEach(elem => elem.classList.add('active'));
+  btnLeave.forEach(elem =>  elem.addEventListener('click', function(){
+    const btnRandom = document.querySelectorAll('.randome-coloda');
+    btnRandom.forEach(elem => elem.classList.add('active'));
+  }))
+})
+)
+
+
+function Stack() {
+  this.count = 0
+  this.storage = {}
+
+  this.push = function(value) {
+      this.storage[this.count] = value
+      this.count++
+  }
+
+  this.pop = function() {
+      if (this.count === 0) return undefined
+      this.count--
+      let result = this.storage[this.count]
+      delete this.storage[this.count]
+      return result
+  }
+
+  this.peek = function() {
+      return this.storage[this.count - 1]
+  }
+  this.size = function() {
+    return this.count
+}
+}
